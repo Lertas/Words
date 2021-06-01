6139135ef0accec3dca3ffc17b9151619d55f376
Lertas <51683084+Lertas@users.noreply.github.com>
2021-06-01 11:37:58 +0300

    Update and rename Askisi3.java to project.java

--- project.java
+++ project.java
@@ -1,6 +1,6 @@
 /*Lertas Georgios icsd11084*/
 
-public class Askisi3 {
+public class project {
 
     public static void main(String[] args) {
         game game = new game();          //kaloume to paixnidi
